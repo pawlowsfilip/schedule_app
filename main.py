614daae8d3e3dfe94b14c6bbfdea7601c8a8f059
pawlowsfilip <pawlows.filip@gmail.com>
2023-10-07 11:17:40 +0200

    Convert time frames to datetime objects in is_available methods and add time_frame_split method

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ if __name__ == '__main__':
     worker_manager1.add_worker('Filip', {'21.07': '8:00-10:00,11:00-15:00', '22.07': '9:00-11:00'},
                                {'21.07': '10:00-11:00', '22.07': '11:00-13:00'}, 'Student')
 
+    worker_manager1.add_worker('Natalia', {'21.07': '21:00-22:00', '22.07': '9:00-11:00'},
+                               {'21.07': '10:00-11:00', '22.07': '11:00-13:00'}, 'Manager')
+
     print(worker_manager1.get_days())
     print(worker_manager1.get_hours('22.07'))
-    #print(worker_manager1.get_available_workers_via_availability('21.07', ))
-
+    worker_list = worker_manager1.get_available_workers_via_availability('21.07', '7:00-23:00')
+    print(worker_list[-1].name)
--- time_utils.py
+++ time_utils.py
@@ -9,12 +9,17 @@ def is_overlap(worker_start, worker_end, required_start, required_end):
     return (worker_start <= required_end) and (worker_end >= required_start)
 
 
+def time_frame_split(time_frame):
+    start, end = time_frame.split('-')
+    return start, end
+
+
 def process_availability(availability_str):
     scopes = availability_str.split(',')
     time_tuples = []
 
     for scope in scopes:
-        start, end = scope.split('-')
+        start, end = time_frame_split(scope)
         start_time = str_to_time(start)
         end_time = str_to_time(end)
         time_tuple = (start_time, end_time)
@@ -28,5 +33,5 @@ def availability(availability_str):
 
 
 def is_available(availability_str, required_start, required_end):
-    return any(is_overlap(worker_start, worker_end, required_start, required_end)
+    return any(is_overlap(worker_start, worker_end, str_to_time(required_start), str_to_time(required_end))
                for worker_start, worker_end in availability(availability_str))
