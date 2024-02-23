573b9c3c54a3911cecba7112e3b7a72118d44821
pawlowsfilip <pawlows.filip@gmail.com>
2024-02-23 19:57:10 +0100

    Corrected the algorithm for scheduler_r

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -4,11 +4,11 @@ from worker import Worker
 
 class Scheduler:
     def __init__(self, variant):
-        worker1 = Worker('Filip', {'21.07': '7:00-10:00'}, {'21.07': '7:00-8:00'}, 'Manager')
+        # worker1 = Worker('Filip', {}, {}, 'Manager')
         worker2 = Worker('Natalia', {'21.07': '10:00-14:00'}, {}, 'Student')
         worker3 = Worker('Ola', {'21.07': '8:00-9:00'}, {}, 'Student')
         worker4 = Worker('Kondziu', {'21.07': '7:00-10:00'}, {'21.07': '10:00-14:00'}, 'Student')
-        wm1 = Worker_Manager(worker1, worker2, worker3, worker4)
+        wm1 = Worker_Manager(worker2, worker3, worker4)
 
         self.variant = variant
         # self.worker_manager = Worker_Manager()
