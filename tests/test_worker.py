6c11930f46648c3e3858a443c713b66a1551da39
pawlowsfilip <pawlows.filip@gmail.com>
2024-03-03 20:29:07 +0100

    Completed unit tests for worker class, changed method's name from _is_overlap to _is_fully_covered

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -3,6 +3,7 @@ from worker import Worker
 import pytest
 
 
+# _init tests____________________________________
 def test_init_with_all_arguments():
     worker = Worker(name='John Doe', availability={'21.07': '8:00-10:00'}, worse_availability={'21.07': '10:00-14:00'},
                     position='Manager')
@@ -20,56 +21,46 @@ def test_init_with_necessary_arguments():
     assert worker.get_position() is None
 
 
-def test_worker_is_available_during_exact_time_frame():
-    worker = Worker(name='John Doe', availability={'21.07': '8:00-10:00'})
-    assert worker.is_available('21.07', '8:00-10:00') is True
+def test_init_with_all_arguments_but_worker_unavailable():
+    worker = Worker(name='John Doe', availability={'21.07': ''}, worse_availability={'21.07': ''},
+                    position='Manager')
+    assert worker.get_name() == 'John Doe'
+    assert worker.get_availability() == {'21.07': ''}
+    assert worker.get_worse_availability() == {'21.07': ''}
+    assert worker.get_position() == 'Manager'
 
 
-def test_worker_is_available_during_time_frame():
-    worker = Worker(name='John Doe', availability={'21.07': '8:00-10:00'})
-    assert worker.is_available('21.07', '8:00-9:30') is True
+# _is_available tests____________________________
+@pytest.mark.parametrize("availability, required_day, required_time, expected",[
+    ({"21.07": "8:00-10:00"}, "21.07", "8:00-9:00", True),  # Available for entire timeframe
+    ({"21.07": "8:00-10:00"}, "21.07", "7:00-9:00", False),  # Available for part of the time frame
+    ({"21.07": "8:00-10:00"}, "21.07", "10:00-11:00", False),  # Not available - with availability
+    ({"21.07": ""}, "21.07", "10:00-11:00", False),  # Not available - without availability
+    ({"21.07": "8:00-10:00"}, "21.07", "8:00-10:00", True),  # Exact match of availability with required time frame
+    ({"21.07": "8:00-9:00"}, "21.07", "6:00-7:00", False),  # Time frame outside the availability
+    ({"22.07": "8:00-9:00"}, "21.07", "8:00-9:00", False),  # Required time frame on different day
+])
+def test_worker_is_available(availability, required_day, required_time, expected):
+    worker = Worker(name='John Doe', availability=availability)
+    assert worker.is_available(required_day, required_time) == expected
 
 
-def test_worker_is_not_available_during_exact_time_frame():
-    worker = Worker(name='John Doe', availability={'21.07': '8:00-10:00'})
-    assert worker.is_available('21.07', '12:00-13:00') is False
+# _is_available_if_needed tests__________________
+@pytest.mark.parametrize("worse_availability, required_day, required_time, expected",[
+    ({"21.07": "8:00-10:00"}, "21.07", "8:00-9:00", True),  # Available for entire timeframe
+    ({"21.07": "8:00-10:00"}, "21.07", "7:00-9:00", False),  # Available for part of the time frame
+    ({"21.07": "8:00-10:00"}, "21.07", "10:00-11:00", False),  # Not available - with availability
+    ({"21.07": ""}, "21.07", "10:00-11:00", False),  # Not available - without availability
+    ({"21.07": "8:00-10:00"}, "21.07", "8:00-10:00", True),  # Exact match of availability with required time frame
+    ({"21.07": "8:00-9:00"}, "21.07", "6:00-7:00", False),  # Time frame outside the availability
+    ({"22.07": "8:00-9:00"}, "21.07", "8:00-9:00", False),  # Required time frame on different day
+])
+def test_worker_is_available_if_needed(worse_availability, required_day, required_time, expected):
+    worker = Worker(name='John Doe', availability={'21.07': ''}, worse_availability=worse_availability)
+    assert worker.is_available_if_needed(required_day, required_time) == expected
 
 
-def test_worker_is_not_available_during_that_day():
-    worker = Worker(name='John Doe', availability={'22.07': '8:00-10:00'})
-    assert worker.is_available('21.07', '12:00-13:00') is False
-
-
-def test_worker_is_available_but_overlap_time_frame():
-    worker = Worker(name='John Doe', availability={'21.07': '8:00-10:00'})
-    assert worker.is_available('21.07', '9:30-11:00') is True
-
-
-def test_worker_is_available_if_needed_during_exact_time_frame():
-    worker = Worker(name='John Doe', availability={'21.07': '8:00-10:00'}, worse_availability={'21.07': '10:00-14:00'})
-    assert worker.is_available_if_needed('21.07', '10:00-14:00') is True
-
-
-def test_worker_is_available_if_needed_during_time_frame():
-    worker = Worker(name='John Doe', availability={'21.07': '8:00-10:00'}, worse_availability={'21.07': '10:00-14:00'})
-    assert worker.is_available_if_needed('21.07', '10:00-13:30') is True
-
-
-def test_worker_is_not_available_if_needed_during_exact_time_frame():
-    worker = Worker(name='John Doe', availability={'21.07': '8:00-10:00'}, worse_availability={'21.07': '10:00-14:00'})
-    assert worker.is_available_if_needed('21.07', '15:00-17:00') is False
-
-
-def test_worker_is_not_available_if_needed_during_that_day():
-    worker = Worker(name='John Doe', availability={'21.07': '8:00-10:00'}, worse_availability={'21.07': '10:00-14:00'})
-    assert worker.is_available_if_needed('22.07', '12:00-13:00') is False
-
-
-def test_worker_is_available_if_needed_but_overlap_time_frame():
-    worker = Worker(name='John Doe', availability={'21.07': '8:00-10:00'}, worse_availability={'21.07': '10:00-14:00'})
-    assert worker.is_available_if_needed('21.07', '13:30-17:00') is True
-
-
+# _str_to_time tests_____________________________
 def test_str_to_time_standard_conversion():
     assert Worker._str_to_time("13:30") == time(13, 30)
 
@@ -85,54 +76,23 @@ def test_str_to_time_invalid_input():
         assert Worker._str_to_time("John Doe") == time(00, 00)
 
 
-def test_is_overlap_exact_hours():
-    worker_start = Worker._str_to_time("8:00")
-    worker_end = Worker._str_to_time("13:00")
-    required_start = Worker._str_to_time("8:00")
-    required_end = Worker._str_to_time("13:00")
-    assert Worker._is_overlap(worker_start, worker_end, required_start, required_end) is True
+# _is_overlap tests______________________________
+@pytest.mark.parametrize("worker_start, worker_end, required_start, required_end, expected", [
+    ("8:00", "10:00", "8:00", "10:00", True),  # Exact match
+    ("7:00", "11:00", "8:00", "10:00", True),  # Worker's availability includes required time frame
+    ("8:00", "9:00", "8:30", "10:00", False),  # Partial overlap at the end
+    ("9:00", "11:00", "8:00", "9:30", False),  # Partial overlap at the start
+    ("11:00", "12:00", "8:00", "9:00", False)  # No overlap
+])
+def test_is_fully_covered(worker_start, worker_end, required_start, required_end, expected):
+    worker_start = Worker._str_to_time(worker_start)
+    worker_end = Worker._str_to_time(worker_end)
+    required_start = Worker._str_to_time(required_start)
+    required_end = Worker._str_to_time(required_end)
+    assert Worker._is_fully_covered(worker_start, worker_end, required_start, required_end) == expected
 
 
-def test_is_overlap_inside_time_frame():
-    worker_start = Worker._str_to_time("8:00")
-    worker_end = Worker._str_to_time("13:00")
-    required_start = Worker._str_to_time("7:00")
-    required_end = Worker._str_to_time("14:00")
-    assert Worker._is_overlap(worker_start, worker_end, required_start, required_end) is True
-
-
-def test_is_overlap_in_first_half():
-    worker_start = Worker._str_to_time("8:00")
-    worker_end = Worker._str_to_time("13:00")
-    required_start = Worker._str_to_time("6:00")
-    required_end = Worker._str_to_time("11:00")
-    assert Worker._is_overlap(worker_start, worker_end, required_start, required_end) is True
-
-
-def test_is_overlap_in_second_half():
-    worker_start = Worker._str_to_time("8:00")
-    worker_end = Worker._str_to_time("13:00")
-    required_start = Worker._str_to_time("11:00")
-    required_end = Worker._str_to_time("16:00")
-    assert Worker._is_overlap(worker_start, worker_end, required_start, required_end) is True
-
-
-def test_is_overlap_last_minute():
-    worker_start = Worker._str_to_time("8:00")
-    worker_end = Worker._str_to_time("13:00")
-    required_start = Worker._str_to_time("12:59")
-    required_end = Worker._str_to_time("19:00")
-    assert Worker._is_overlap(worker_start, worker_end, required_start, required_end) is True
-
-
-def test_is_not_overlap():
-    worker_start = Worker._str_to_time("8:00")
-    worker_end = Worker._str_to_time("13:00")
-    required_start = Worker._str_to_time("13:01")
-    required_end = Worker._str_to_time("16:00")
-    assert Worker._is_overlap(worker_start, worker_end, required_start, required_end) is False
-
-
+# _time_frame_split tests________________________
 def test_time_frame_split_correctly():
     time_frame = "12:00-13:00"
     assert Worker._time_frame_split(time_frame) == ("12:00", "13:00")
@@ -143,6 +103,46 @@ def test_time_frame_split_one_hour():
     with pytest.raises(ValueError):
         assert Worker._time_frame_split(time_frame)
 
-"""
-TBD
-"""
+
+# _process_availability tests____________________
+def test_process_availability_correct_one_frame():
+    worker = Worker(name='John Doe', availability={})
+    availability_str = '8:00-10:00'
+    expected_output = [(worker._str_to_time('8:00'), worker._str_to_time('10:00'))]
+    assert worker._process_availability(availability_str) == expected_output
+
+
+def test_process_availability_correct_more_than_one_frame():
+    worker = Worker(name='John Doe', availability={})
+    availability_str = '8:00-10:00,11:00-12:00'
+    expected_output = [(worker._str_to_time('8:00'), worker._str_to_time('10:00')),
+                       (worker._str_to_time('11:00'), worker._str_to_time('12:00'))]
+    assert worker._process_availability(availability_str) == expected_output
+
+
+def test_process_availability_empty_string():
+    worker = Worker(name='John Doe', availability={})
+    availability_str = ''
+    expected_output = []
+    assert worker._process_availability(availability_str) == expected_output
+
+
+def test_process_availability_incorrect():
+    worker = Worker(name='John Doe', availability={})
+    availability_str = '9:00-10:00'
+    expected_output = [(worker._str_to_time('8:00'), worker._str_to_time('10:00'))]
+    assert worker._process_availability(availability_str) != expected_output
+
+
+def test_process_availability_incorrect_one_hour():
+    worker = Worker(name='John Doe', availability={})
+    availability_str = 'invalid input'
+    with pytest.raises(ValueError):
+        assert worker._process_availability(availability_str)
+
+
+def test_process_availability_incorrect_not_a_string():
+    worker = Worker(name='John Doe', availability={})
+    availability_str = 9    # int doesn't have a 'split'
+    with pytest.raises(AttributeError):
+        assert worker._process_availability(availability_str)
