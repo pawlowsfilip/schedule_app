a1fcc84b5dc34f05628a7d0330dbfc4286cb2536
pawlowsfilip <pawlows.filip@gmail.com>
2024-03-11 21:15:40 +0100

    Added unit tests for scheduler_r

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -6,13 +6,16 @@ from abc import ABC, abstractmethod
 
 class Scheduler(ABC):
     def __init__(self, variant):
-        worker1 = Worker('Filip', {'21.07': '8:00-10:00'}, {'21.07': '7:00-10:00'}, 'Manager')
-        worker2 = Worker('Natalia', {'21.07': '8:00-10:00'}, {}, 'Regular')
-        worker3 = Worker('Ola', {'21.07': '8:00-9:00'}, {'21.07': '10:00-14:00'}, 'Student')
-        worker4 = Worker('Kondziu', {'21.07': '10:00-14:00'}, {'21.07': '10:00-14:00'}, 'Student')
-        wm1 = Worker_Manager(worker1, worker2, worker3, worker4)
-        wm1.set_position_priorities({'Manager': 1, 'Regular': 2, "Student": 3})
-        self.worker_manager = wm1
+        """
+        For testing
+        """
+        # worker1 = Worker('Filip', {'21.07': '8:00-10:00'}, {'21.07': '7:00-10:00'}, 'Manager')
+        # worker2 = Worker('Natalia', {'21.07': '8:00-10:00'}, {}, 'Regular')
+        # worker3 = Worker('Ola', {'21.07': '8:00-9:00'}, {'21.07': '10:00-14:00'}, 'Student')
+        # worker4 = Worker('Kondziu', {'21.07': '10:00-14:00'}, {'21.07': '10:00-14:00'}, 'Student')
+        # wm1 = Worker_Manager(worker1, worker2, worker3, worker4)
+        # wm1.set_position_priorities({'Manager': 1, 'Regular': 2, "Student": 3})
+        # self.worker_manager = wm1
 
         self.variant = variant
         self.schedule = {}
