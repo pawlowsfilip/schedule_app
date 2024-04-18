7f26fc112d691734e990f2412512724df6ed9d70
pawlowsfilip <pawlows.filip@gmail.com>
2024-04-18 21:39:07 +0200

    added new unit test for scheduler_s

--- scheduler_factory.py
+++ scheduler_factory.py
@@ -6,12 +6,12 @@ class SchedulerFactory:
     @staticmethod
     def get_scheduler(variant, **kwargs):
         if variant == 'R':
-            """
-            For testing
-            """
-            accuracy = 1.0
-            allocation = {"7:00-10:00": 1, "10:00-14:00": 2}
-            return Scheduler_r(variant, accuracy, allocation)
+            # """
+            # For testing
+            # """
+            # accuracy = 1.0
+            # allocation = {"7:00-10:00": 1, "10:00-14:00": 2}
+            return Scheduler_r(variant, **kwargs)
         if variant == 'S':
             return Scheduler_s(variant, **kwargs)
         else:
